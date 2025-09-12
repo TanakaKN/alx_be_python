a841bf94d91559b09f7b4150a447efb5c4c8fec4
Tanaka Ndopo <tkndopo@yahoo.com>
2025-09-12 12:06:29 +0200

    added a python script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum_result = number1 + number2
+print("Sum of", number1, "and", number2, "is", sum_result)
+
+# Product
+product_result = number1 * number2
+print("Product of", number1, "and", number2, "is", product_result)
+

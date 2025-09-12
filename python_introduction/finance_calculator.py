d5b1901f19fd395c6f47f626bdc316adca224e93
Tanaka Ndopo <tkndopo@yahoo.com>
2025-09-12 13:33:38 +0200

    Added a Personal Finance Calculator script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+users_monthly_income = input("Enter your monthly income: ")
+users_monthly_expenses = input("Enter your total monthly expenses: ")
+
+monthly_savings = users_monthly_income - users_monthly_expenses
+
+annual_interest_rate = 0.05
+
+Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate)
+
+print(f"Your monthly savings are {monthly_savings}.")
+
+print(f"Projected savings after one year, with interest, is: {Projected_Savings}")

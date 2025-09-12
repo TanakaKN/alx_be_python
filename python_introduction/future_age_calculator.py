1f1daf248d29bc7c2fe8be1b498b072e2727efdb
Tanaka Ndopo <tkndopo@yahoo.com>
2025-09-12 13:00:47 +0200

    Added a User Input Age calculator script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_year = 2023
+future_year = 2050
+current_age = int(input("Enter your current age: "))
+future_age = current_age + (future_year - current_year)
+print(f"In {future_year}, you will be {future_age} years old.")

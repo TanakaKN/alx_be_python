2bd4bf83b2bd82a9d7c8bd879f448ae7b658f7ae
TanakaKN <tkndopo@yahoo.com>
2025-09-21 18:30:08 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,7 @@
-number = int(input("Enter a number to see its multiplication table:."))
+# Asking the user for a number
+number = int(input("Enter a number to see its multiplication table: "))
 
-for i in range(1, 11):
-    print(number, "*", i, "=", number*i)
+# Using a for loop to generate the table from 1 to 10
+for i in range(1, 11):  # range(1, 11) means numbers 1 through 10
+    result = number * i
+    print(f"{number} x {i} = {result}")

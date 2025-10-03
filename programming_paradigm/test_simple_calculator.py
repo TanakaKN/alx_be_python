bbf1c09c81c87a2bbc95c76ac8cccf04fe07c047
Tanaka Ndopo <tkndopo@yahoo.com>
2025-10-03 20:38:23 +0200

    Fixed some bugs caught by the unit test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,29 +4,30 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(4, 4), 8)
-        self.assertEqual(self.calc.add(0, 4), 4)
-        self.assertEqual(self.calc.add(5, 1), 6)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(4, 0), 4)
-        self.assertEqual(self.calc.subtract(4, 4), 0)
-        self.assertEqual(self.calc.subtract(5, 4), 1)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(0, 4), 0)
-        self.assertEqual(self.calc.multiply(4, 4), 16)
-        self.assertEqual(self.calc.multiply(5, 4), 20)
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(4, 4), 1)
-        self.assertEqual(self.calc.divide(16, 4), 4)
-        with self.assertRaises(ZeroDivisionError):
-            self.calc.divide(4, 0)
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertEqual(self.calc.divide(0, 5), 0)
 
-if __name__ == "__main__":
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(5, 0)
+
+if __name__ == '__main__':
     unittest.main()

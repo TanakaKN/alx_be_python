6dd67e42ef3ae7ed6274085ba4e12e2e1147238a
Tanaka Ndopo <tkndopo@yahoo.com>
2025-10-03 18:03:58 +0200

    Objected oriented programming files

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,18 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
+    def __init__(self, account_balance=0):
         self.account_balance = account_balance
-        self.amount= 0
 
     def deposit(self, amount):
-        self.amount = amount
-        self.account_balance += self.amount
-        return self.account_balance
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        return False
 
     def withdraw(self, amount):
-        self.amount = self.amount
-        if amount > self.account_balance:
-            print("insufficient funds.")
-            return self.account_balance
-        else:
-            self.account_balance += self.amount
-            return self.account_balance
-             
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
 
     def display_balance(self):
-        print(f"Your current balance is {self.account_balance}") 
-           
+        print(f"Current Balance: ${self.account_balance:.2f}")

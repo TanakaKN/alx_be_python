b5f2532e064075ef272238ce4dd19af00e61e1ab
Tanaka Ndopo <tkndopo@yahoo.com>
2025-10-26 16:02:30 +0200

    fixed the folder name

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,14 +2,13 @@ class Book:
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
-        self.year = year
+        self.year = int(year)
 
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"  
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}','{self.year}')"  
-    
+        return f"Book({repr(self.title)}, {repr(self.author)}, {self.year})"
+
     def __del__(self):
-        print( "Deleting {self.title}")
-        self.close()
+        print(f"Deleting {self.title}")
